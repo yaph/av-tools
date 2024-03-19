fe2a3f10e823fa3e34112fa6bae52664c5391c22
Ramiro Gómez <code@ramiro.org>
2024-03-19 23:47:21 +0100

    Add description and usage info.

--- merge_av.sh
+++ merge_av.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# ./merge_av.sh audio.mp3 video.mp4
+# This script combines a video file with no sound and an audio file using ffmpeg,
+# ensuring that if the audio is longer, the last frame of the video is shown until the audio ends,
+# and if the video is longer, silence follows after the audio finishes.
 set -euo pipefail
+
+# Check if correct number of arguments provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <audio> <video>"
+    exit 1
+fi
 
 audio=$1
 video=$2
 
-out="${video%.*}"_audio_merged."${video##*.}"
+out="${video%.*}".av."${video##*.}"
 
 ffmpeg -i "$audio" -i "$video" -c:v copy -c:a aac -map 0:a:0 -map 1:v:0 "$out"
