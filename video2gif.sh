7f9223172939ee96585f40244b599b56962c23e5
Ramiro Gómez <code@ramiro.org>
2025-02-01 14:34:10 +0100

    Add rate argument

--- video2gif.sh
+++ video2gif.sh
@@ -6,10 +6,11 @@ set -euo pipefail
 video=$1
 video_size=${2:-800}
 padding_size=${3:-10}
+rate=${4:-10}
 
 output="${video%.*}".gif
 
 # Convert video to GIF
-ffmpeg -i "$video" -vf "fps=10,setpts=0.5*PTS,scale=${video_size}-${padding_size}*2:-1:flags=lanczos,pad=width=iw+${padding_size}*2:height=ih+${padding_size}*2:x=${padding_size}:y=${padding_size}:color=black" -c:v gif "$output"
+ffmpeg -i "$video" -r "$rate" -vf "setpts=0.5*PTS,scale=${video_size}-${padding_size}*2:-1:flags=lanczos,pad=width=iw+${padding_size}*2:height=ih+${padding_size}*2:x=${padding_size}:y=${padding_size}:color=black" -c:v gif "$output"
 
 echo "Conversion complete. Output saved as $output"
